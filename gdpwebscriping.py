7e2c874f8e730c6b48b5e2d5c73228df96f5109b
kamil Kuczyński <kamil.kuczynski4+github@gmail.com>
2023-01-23 23:29:44 +0100

    Downloading tables from Wikipedia and saving them in csv's format

--- gdpwebscriping.py
+++ gdpwebscriping.py
@@ -12,32 +12,10 @@ response = requests.get(wikiurl)
 
 # parse data from the html into a beautifulsoup object
 soup = BeautifulSoup(response.text, 'html.parser')
-indiatable=soup.find_all('table',{'class':"wikitable"})
+
+tables = soup.find_all('table', {'class': 'wikitable'})
 
 
-df=pd.read_html(str(indiatable))
-#print(df)
-
-# rename columns for ease
-
-
-# convert list to dataframe
-#for i in range()
-df=pd.DataFrame(df[0])
-
-df = df.rename(columns={"Country (or dependent territory)": "Country"})
-print(df.head(50))
-
-df.to_csv('gdp_per_capita.csv', index=True, mode='a', quoting=csv.QUOTE_NONNUMERIC)
-
-'''
-import csv
-with open("gdp_per_capita.csv", "a") as csv_file:
-    writer = csv.writer(csv_file)
-    for row in df:
-        cols = row.find_all('td') # in html a column is represented by the tag <td>
-        language_name = cols[1].getText() # store the value in column 3 as color_name
-        annual_average_salary = cols[3].getText() # store the value in column 4 as color_code
-        ## now we will write data to the file
-        writer.writerow([language_name, annual_average_salary])
-'''
+for i, table in enumerate(tables):
+    df = pd.read_html(str(table))[0]
+    df.to_csv(f'gdp_per_capita_{i}.csv', index=True, quoting=csv.QUOTE_NONNUMERIC)
