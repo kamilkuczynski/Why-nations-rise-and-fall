055835e7f538336fc206a46aaafdb19ea7a635d7
kamil Kuczyński <kamil.kuczynski4+github@gmail.com>
2023-03-05 18:22:27 +0100

    add shape and dtypes

--- 3_data_wrangling.py
+++ 3_data_wrangling.py
@@ -7,6 +7,7 @@ import seaborn as sns
 from scipy import stats
 
 
+
 def create_bar_chart(df, n, title, value_column):
     # get the first n rows of the dataframe
     df_first_n = df.head(n)
@@ -45,55 +46,62 @@ file_name = "C:/Users/kuczy/Documents/GitHub/Why-nations-rise-and-fall/gdp_per_c
 
 df = pd.read_csv(file_name)
 
+print("\nPrint the first few rows of the DataFrame")
+print(df.head())
+
 df.rename(columns={df.columns[0]: "Country"}, inplace= True)
 
 duplicate_rows = df.duplicated()
-print(f"This data contains {duplicate_rows.sum()} duplicated rows.\n")
+print(f"This data contains {duplicate_rows.sum()} duplicated rows.")
 
-print("Checking how many rows are there in the dataset?")
-#print(len(df), '\n')
+print("\nPrint the DataFrame's shape")
+print(df.shape)
 
-print("\n Find the missing values for all columns. \n")
+print("\nPrint the DataFrame's data types")
+print(df.dtypes)
+
+print("\nChecking how many rows are there in the dataset?")
+print(len(df), '\n')
+
+print("\nFind the missing values for all columns.")
 missing_values_per_column = df.isnull().sum()
-#print(missing_values_per_column)
+print(missing_values_per_column)
 
-print("Checking which columns has more than 100 mising values \n")
+print("\nChecking which columns has more than 100 mising values")
 columns_with_more_than_100_missing_values = missing_values_per_column[missing_values_per_column > 100].index
-#print(columns_with_more_than_100_missing_values)
+print(columns_with_more_than_100_missing_values)
 
-print("Dropping columns that have more than 100 missing values \n")
+print(" \nDropping columns that have more than 100 missing values")
 df = df.drop(columns=columns_with_more_than_100_missing_values)
-#print(df)
+print(df.head())
 
-print("Checking which columns are not needed\n")
+print("\nChecking which columns are not needed")
 #print(df.columns)
 df = df.drop(columns=['Indicator Name', 'Indicator Code'])
-#print(df.columns)
+print(df.columns)
 
 mask = df['2021'].isnull()
 #print(mask)
 #print(df.loc[mask, :])
 
-print("Removing countries where data for 2020 and 2021 are missing \n")
+print("\nRemoving countries where data for 2020 and 2021 are missing")
 mask = df['2020'].isnull() & df['2021'].isnull()
 #print(df.loc[mask, :]) #checking the data
 
-# Drop countries without data
+print("\nDrop countries without data")
 df.drop(df[mask].index, inplace = True)
 df_2020_2021 = df.loc[:, ["Country", "2020", "2021"]] #checking data
-#print(df_2020_2021)
 
-# changing datatype from string to float
+print("\nChanging datatype from string to float")
 cols = [str(year) for year in range(1990, 2022)]
 df[cols] = df[cols].apply(lambda x: x.str.replace(',', '.')).astype(float)
 
-print("Count growth of all countries from 1990 to 2021 \n")
+print("\nCount growth of all countries from 1990 to 2021")
 #fullfill empty series in column 1990 with .bfill() method, which fills missing values with the next non-null value in the column
 df = df.bfill( axis= 1)
 df = df.ffill( axis= 1)
 
-#print(tabulate(df, headers='keys'))
-#add column with growth from 1990 to 20221
+print("\nAdd column with growth from 1990 to 20221")
 df['2021'] = df['2021'].astype(float)
 df['1990'] = df['1990'].astype(float)
 df['Growth'] = round(df["2021"]/df["1990"] - 1, 2)
@@ -101,14 +109,14 @@ df['Growth'] = round(df["2021"]/df["1990"] - 1, 2)
 # Print 5 random rows from the sorted dataframe
 #print(df.sample(5))
 
-#plot a bar chrt for top 10 exonomies:
+#plot a bar chrt for top 10 excnomies:
 #create_bar_chart(df=df, n=31, title="Top 10 best growing countries", value_column="Growth")
 
 # float_format is set to a function that formats floating point numbers with a comma as a thousands separator,
 # and with no decimal places to read more comfortable data
 pd.options.display.float_format = '{:,.2f}'.format
 
-# Load the 'area.csv' file and skip first 4 rows
+print("\nLoad the 'area.csv' file and skip first 4 rows")
 file_area = "area.csv"
 df_area = pd.read_csv(file_area, skiprows=4, encoding='UTF-8')
 
@@ -116,13 +124,13 @@ df_area = pd.read_csv(file_area, skiprows=4, encoding='UTF-8')
 #print("Print the area df:\n", df_area)
 
 
-# Rename the column '2020' to 'Area' in the dataframe 'df_area'
+print("\nRename the column '2020' to 'Area' in the dataframe 'df_area'")
 df_area.rename(columns={"2020" : "Area"}, inplace=True)
 
-# Print the modified dataframe
-#print(df_area)
+print("\nPrint the modified dataframe")
+print(df_area.head())
 
-print('Merge the dataframes \'df\' and \'df_area\' on the common column \'Country Code\'')
+print('\nMerge the dataframes \'df\' and \'df_area\' on the common column \'Country Code\'')
 
 # with the type of merge being 'left'
 df = df.merge(df_area[["Country Code", 'Area']], on='Country Code', how='left')
@@ -137,19 +145,19 @@ df['Area'] = df['Area'].astype(str).str.replace(',', '.').astype(float)
 # Check if column type has changed
 #print(df['Area'].dtypes)
 
-# Create scatter plot of "Growth" and "Area"
-sns.regplot(x="Area", y="Growth", data=df)
+print("\nCreate scatter plot of 'Growth' and 'Area'")
+sns.regplot(x="Area", y="Growth", data=df, color="#1A00F3")
 plt.ylim(0,)
-#plt.show()
+plt.show()
 
-# Check correlation between Growth of country and its size and P-value of Growth and Area to know the significant of the correlation estimate:
+print("\nCheck correlation between Growth of country and its size and P-value of Growth and Area to know the significant of the correlation estimate:")
 print('\n', df[["Growth", "Area"]].corr())
 
 df_clean= df.dropna(subset=['Area']) # removing countries with nan value in Area column
 
 pearson_coef, p_value = stats.pearsonr(df_clean['Area'], df_clean['Growth'])
 
-print("The Pearson Correlation Coefficient is", round(pearson_coef, 3), " with a P-value of P =", round(p_value, 4))
+print("\nThe Pearson Correlation Coefficient is", round(pearson_coef, 3), " with a P-value of P =", round(p_value, 4))
 
 '''
 Based on the code, the Pearson Correlation Coefficient between the Growth and Area columns is 0.055. 
@@ -170,17 +178,17 @@ df = df.merge(df_population[["Country Code", 'Population']], on='Country Code', 
 # Check data type of column and column "Population"
 #print(df.dtypes)
 
-# Check correlation between Growth of country and its size of Population
-print("\n", df[["Growth", "Population"]].corr())
+print("\nCheck correlation between Growth of country and its size of Population")
+print(df[["Growth", "Population"]].corr())
 
 pearson_coef, p_value = stats.pearsonr(df['Population'], df['Growth'])
 
-print("The Pearson Correlation Coefficient is", round(pearson_coef, 3), " with a P-value of P =", round(p_value, 4), '\n')
+print("\nThe Pearson Correlation Coefficient is", round(pearson_coef, 3), " with a P-value of P =", round(p_value, 4), '\n')
 
-# Create scatter plot of "Growth" and "Population"
-sns.regplot(x="Population", y="Growth", data=df)
+print("\nCreate scatter plot of 'Growth' and 'Population'")
+sns.regplot(x="Population", y="Growth", data=df, color="#1A00F3")
 plt.ylim(0,)
-#plt.show()
+plt.show()
 
 '''
 Conclusion: 
@@ -223,11 +231,12 @@ mean = mean.rename(columns={'ISO_Code': 'Country Code'})
 # Merge the mean dataframe with the main dataframe on the "Country Code" column.
 df = df.merge(mean.reset_index(), how="left", on="Country Code")
 
-# Rename the "Economic Freedom Summary Index" column to "Average Economic Freedom".
+print('\nRename the "Economic Freedom Summary Index" column to "Average Economic Freedom.')
 df = df.rename(columns={"Economic Freedom Summary Index": "Average Economic Freedom"})
 
-# Calculate the correlation between the "Growth" and "Average Economic Freedom" columns.
+print('\nCalculate the correlation between the "Growth" and "Average Economic Freedom" columns.')
 correlation_with_economic_freedom = df[["Growth","Average Economic Freedom"]].corr()
+
 # Print the correlation results.
 print('\n', correlation_with_economic_freedom)
 
@@ -236,17 +245,25 @@ df_clean = df.dropna(subset=['Average Economic Freedom']) # removing countries w
 pearson_coef, p_value = stats.pearsonr(df_clean['Average Economic Freedom'], df_clean['Growth'])
 
 print("The Pearson Correlation Coefficient is", round(pearson_coef, 3), " with a P-value of P =", round(p_value, 4), '\n')
+sns.regplot(x="Average Economic Freedom", y="Growth", data=df, color="#1A00F3")
+plt.ylim(0,)
+plt.show()
 
+print('\n, Correlation of with economic freedom with gdp per capita for 2021 year')
+
+#df_clean = df.dropna(subset=['Average Economic Freedom']) # removing countries with nan value in Average Economic Freedom column
+
+pearson_coef, p_value = stats.pearsonr(df_clean['Average Economic Freedom'], df_clean['2021'])
+
+print("The Pearson Correlation Coefficient is", round(pearson_coef, 3), " with a P-value of P =", round(p_value, 4), '\n')
+
+sns.regplot(x="Average Economic Freedom", y="2021", data=df, color="#1A00F3")
+plt.ylim(0,)
+plt.show()
 
 
 # Print the final dataframe.
-#print(df)
-
-#print(tabulate(df_economy_freedom, headers="keys"))
+print(tabulate(df.head(), headers="keys"))
 
 #save df to csv
 df.to_csv("gdp_per_capita_ppp_constant_2017_modified.csv", index=False)
-
-
-
-
